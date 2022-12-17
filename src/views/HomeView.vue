d5c52b5000c3f5e97426a45c37ca298f5e4e038e
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-12-17 23:57:07 +0900

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <TodoInput msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent } from 'vue';
+import TodoInput from '@/components/TodoInput.vue'; // @ is an alias to /src
 
 export default defineComponent({
-  name: "HomeView",
+  name: 'TodoInput',
   components: {
-    HelloWorld,
+    TodoInput,
   },
 });
 </script>
