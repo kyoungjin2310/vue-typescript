c123967832930e37c9f30b88255fa0e048f2a18f
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-12-18 23:38:47 +0900

    typescript setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,21 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': 'off',
+    // "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    // "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'always',
+      },
+    ],
   },
 };
