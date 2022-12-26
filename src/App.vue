083132374e41af17ef4eeb1e439f5d68750959cc
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-12-26 23:36:14 +0900

    feat: [localStorage - ts] localStorage fetch

--- src/App.vue
+++ src/App.vue
@@ -35,21 +35,38 @@ import { defineComponent } from 'vue';
 import TodoInput from './components/TodoInput.vue';
 import TodoListItem from './components/TodoListItem.vue';
 
+const STORAGE_KEY = 'vue-todo-ts-vi';
+const storage = {
+  save(todos: string[]) {
+    const parse = JSON.stringify(todos);
+    localStorage.setItem(STORAGE_KEY, parse);
+  },
+  fetch() {
+    //localStorage는 json이라 빈배열을 string으로 바꿔줌
+    const todoItems = localStorage.getItem(STORAGE_KEY) || '[]';
+    const result = JSON.parse(todoItems);
+    return result;
+  },
+};
+
 export default defineComponent({
   components: { TodoInput, TodoListItem },
   data() {
     return {
       todoTxt: '',
       test: '',
+      todoItems: [],
     };
   },
   methods: {
-    updateTodoText(val: string) {
+    updateTodoText(val: string[]) {
       this.todoTxt = val;
     },
     addTodoItem() {
       const val = this.todoText;
-      localStorage.setItem(val, val);
+      // localStorage.setItem(val, val);
+      this.todoItems.push(val);
+      storage.save(this.todoItems);
       this.initTodoText();
     },
     initTodoText() {
@@ -58,6 +75,12 @@ export default defineComponent({
     updateVmodel(val: string) {
       this.test = val;
     },
+    fetchTodoItems() {
+      this.todoItems = storage.fetch();
+    },
+  },
+  created() {
+    this.fetchTodoItems();
   },
 });
 </script>
